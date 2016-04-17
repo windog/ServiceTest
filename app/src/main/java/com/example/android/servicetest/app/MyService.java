03a4b7f42212e414817f690d0cb23d4ffbe07e17
windog <windiscall@gmail.com>
2016-04-17 14:36:57 +0800

    use a new method to create a notification  Notification.builder().build();

--- app/src/main/java/com/example/android/servicetest/app/MyService.java
+++ app/src/main/java/com/example/android/servicetest/app/MyService.java
@@ -1,11 +1,15 @@
 package com.example.android.servicetest.app;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
+
+import com.example.android.servicetest.R;
 
 /**
  * Created by windog on 2016/4/16.
@@ -37,6 +41,16 @@ public class MyService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+        Intent notificationIntent = new Intent(this,MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,notificationIntent,0);
+        Notification notification = new Notification.Builder(MyService.this)
+                                    .setContentTitle("this is title")
+                                    .setSmallIcon(R.drawable.as)
+                                    .build();
+        startForeground(1,notification);
+
+        Log.d("Myservice","onCreate executed");
+
     }
 
     @Override
